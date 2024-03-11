2f6b4b58e4ff714ad4d55006f5ef95f211addf07
ubaidilahalbayu <ubaidilahalbayu@gmail.com>
2024-03-11 11:52:00 +0700

    api done

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -9,48 +9,83 @@ class Api extends RestController {
     {
         // Construct the parent class
         parent::__construct();
+
+        // Load model 
+        $this->load->model('user_Model'); 
+        $this->load->library('Authorization_Token');
+    }
+
+    public function genJWTCI3_get(){
+        $token_data['id'] = "002";//$user_id;
+        $token_data['username'] = "@mail";//$username; 
+        $tokenData = $this->authorization_token->generateToken($token_data);
+        $final = array();
+        $final['access_token'] = $tokenData;
+        $final['status'] = true;
+
+        $this->response($final, RESTController::HTTP_OK); 
+    }
+    public function tokenCI3_get() : Returntype {
+        $headers = $this->input->request_headers(); 
+		if (isset($headers['Authorization'])) {
+            $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+            if ($decodedToken['status'])
+            {
+                $this->response($decodedToken);
+            }
+            else {
+                $this->response($decodedToken);
+            }
+		}
+		else {
+			$this->response(['Authentication failed'], RESTController::HTTP_OK);
+		}
     }
 
     public function users_get()
     {
-        // Users from a data store e.g. database
-        $users = [
-            ['id' => 0, 'name' => 'John', 'email' => 'john@example.com'],
-            ['id' => 1, 'name' => 'Jim', 'email' => 'jim@example.com'],
-        ];
+        $headers = $this->input->request_headers(); 
+		if (isset($headers['Authorization'])) {
+            $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+            if ($decodedToken['status'])
+            {
+                $where_data = $this->get( 'where_data' );//must be an array
+                $users = array();
 
-        $id = $this->get( 'id' );
-
-        if ( $id === null )
-        {
-            // Check if the users data store contains users
-            if ( $users )
-            {
-                // Set the response and exit
-                $this->response( $users, 200 );
+                if ( $where_data === null )
+                {
+                    $users = $this->user_Model->get_users();
+                }
+                else
+                {
+                    $users = $this->user_Model->get_user_where($where_data);
+                }
+                
+                // Check if the users data store contains users
+                if ( count($users) > 0 )
+                {
+                    // Set the response and exit
+                    $this->response( [
+                        'status' => true,
+                        'message' => 'Users available',
+                        'data' => $users
+                    ], 200 );
+                }
+                else
+                {
+                    // Set the response and exit
+                    $this->response( [
+                        'status' => false,
+                        'message' => 'No users were found'
+                    ], 404 );
+                }
             }
-            else
-            {
-                // Set the response and exit
-                $this->response( [
-                    'status' => false,
-                    'message' => 'No users were found'
-                ], 404 );
+            else {
+                $this->response($decodedToken);
             }
-        }
-        else
-        {
-            if ( array_key_exists( $id, $users ) )
-            {
-                $this->response( $users[$id], 200 );
-            }
-            else
-            {
-                $this->response( [
-                    'status' => false,
-                    'message' => 'No such user found'
-                ], 404 );
-            }
-        }
+		}
+		else {
+			$this->response(['Authentication failed'], RESTController::HTTP_OK);
+		}
     }
 }
